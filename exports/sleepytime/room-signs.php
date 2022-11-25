57e8b4687df25604d13fb7c4462fa3e6204f0e72
WafflesMcDuff <goldjd@gmail.com>
2022-11-25 13:24:18 -0500

    Update building names for room signs

--- exports/sleepytime/room-signs.php
+++ exports/sleepytime/room-signs.php
@@ -129,7 +129,7 @@
                 $res = $UPLINK->query($sql);
                 echo "<div class='roomsign' style=''>";
                 echo '<p><button class="button" id="printPageButton" style="width: 100%;" onClick="window.print();">Print</button></p>';
-                echo "<center class='center'><font face='Orbitron' size=15><br>" . str_replace('tweede gebouw', 'Geitenkamp', $building) . "<br>$room<br><br></font></center>";
+                echo "<center class='center'><font face='Orbitron' size=15><br>" . str_replace('tweede gebouw', 'Zonnedauw', $building) . "<br>$room<br><br></font></center>";
                 echo "<table>";
                 echo "<th><center>Name</center></th>
                 <!--<th><center>Eating Location</center></th>-->
