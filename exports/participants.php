d35f9edb3fa66174507d5ece3db7938178162c2b
WafflesMcDuff <goldjd@gmail.com>
2023-05-12 22:33:56 +0200

    Add E-mail export button

--- exports/participants.php
+++ exports/participants.php
@@ -32,30 +32,25 @@ include_once('./current-players.php');
       color: white;
     }
     
-
-    td,
-    th {
+    td, th {
       border: 1px solid #dddddd;
       text-align: left;
       padding: 2px 4px;
       font-size: 16px;
     }
-@media screen {
-  
-
-    tr:nth-child(even) {
+    
+    @media screen {
+      tr:nth-child(even) {
       background-color: #555555;
       color: white;
     }
 
     tr:nth-child(odd) {
-      /* background-color: #555555; */
       color: white;
     }
   }
-
-    .button {
-      background-color: #4CAF50;
+  .button {
+    background-color: #4CAF50;
       border: none;
       color: white;
       padding: 15px 32px;
@@ -66,12 +61,24 @@ include_once('./current-players.php');
       margin: 4px 2px;
       cursor: pointer;
     }
+
+    .hidden-text {
+      display: none;
+    }
    
     @media print {
       #printPageButton {
         display: none;
       }
-
+      #CopyEmailButton {
+        display: none;
+      }
+      #SortBy{
+        display: none;
+      }
+      #SummaryTable {
+        display: none;
+      }
 
       * {
         -webkit-print-color-adjust: exact;
@@ -114,12 +121,12 @@ include_once('./current-players.php');
     $tableSort = urldecode($_GET["sort"]);
   }
   else {
-    $tableSort = "register_date desc";
+    $tableSort = "oc_fn asc";
   }
   $sql2 = "SELECT title FROM jml_eb_events where id = $EVENTID;";
   $res2 = $UPLINK->query($sql2);
   $row2 = mysqli_fetch_array($res2);
-  $sql = "select r.id, r.first_name as oc_fn, r.register_date, tussenvoegsel.field_value as oc_tv,
+  $sql = "select r.id, r.first_name as oc_fn, r.register_date, r.email as email, tussenvoegsel.field_value as oc_tv,
   r.last_name as oc_ln, substring_index(charname.field_value,' - ',1) as ic_name, soort_inschrijving.field_value as type
 from joomla.jml_eb_registrants r
 join joomla.jml_eb_field_values charname on (charname.registrant_id = r.id and charname.field_id = 21 )
@@ -128,7 +135,7 @@ left join joomla.jml_eb_field_values soort_inschrijving on (soort_inschrijving.r
 where soort_inschrijving.field_value = 'Speler' AND r.event_id = $EVENTID and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal' OR r.payment_method = 'os_bancontact')) OR
 (r.published in (0,1) AND r.payment_method = 'os_offline'))
 UNION
-select r.id, r.first_name as oc_fn, r.register_date, tussenvoegsel.field_value as oc_tv,
+select r.id, r.first_name as oc_fn, r.register_date, r.email as email, tussenvoegsel.field_value as oc_tv,
 r.last_name as oc_ln, NULL as ic_name, soort_inschrijving.field_value as type
 from joomla.jml_eb_registrants r
 left join joomla.jml_eb_field_values tussenvoegsel on (tussenvoegsel.registrant_id = r.id and tussenvoegsel.field_id = 16)
@@ -138,6 +145,12 @@ WHERE soort_inschrijving.field_value != 'Speler' AND r.event_id = $EVENTID and (
   $res = $UPLINK->query($sql);
   $row_count = mysqli_num_rows($res);
 
+
+  $sql4 = "select r.email as email from joomla.jml_eb_registrants r
+where r.event_id = $EVENTID and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal' OR r.payment_method = 'os_bancontact')) OR
+(r.published in (0,1) AND r.payment_method = 'os_offline'))";
+  $res4 = $UPLINK->query($sql4);
+
   $sql3 = "select COUNT(r.id) as count, soort_inschrijving.field_value as type
   from joomla.jml_eb_registrants r       
   left join joomla.jml_eb_field_values tussenvoegsel on (tussenvoegsel.registrant_id = r.id and tussenvoegsel.field_id = 16)
@@ -146,11 +159,11 @@ WHERE soort_inschrijving.field_value != 'Speler' AND r.event_id = $EVENTID and (
   (r.published in (0,1) AND r.payment_method = 'os_offline'))
   GROUP BY soort_inschrijving.field_value";
   $res3 = $UPLINK->query($sql3);
-  echo "<button class=\"button\" id=\"printPageButton\" style=\"width: 100px;\" onClick=\"location.href='/eoschargen/exports/participants.php?sort=oc_fn%20asc'; window.print();\">Print</button>";
+  echo "<button class=\"button\" id=\"printPageButton\" style=\"width: 100px;\" onClick=\"window.print();\">Print</button>";
   echo '<font size="5">Participants for ' . $row2['title'] . ' - '
     . "($row_count participants)</font>";
   
-    echo '<table style="width:30%">';
+    echo '<div id="SummaryTable"><table style="width:30%">';
   echo '<th width="10%">Soort Inschrijf</th>';
   echo '<th width="10%">Aantal Deelneemers</th>';
   echo '<th width="80%">&nbsp;</th><th>&nbsp;</th>';
@@ -162,33 +175,55 @@ WHERE soort_inschrijving.field_value != 'Speler' AND r.event_id = $EVENTID and (
       echo "<td>&nbsp;</td><td>&nbsp;</td>";
       echo "</tr>";
     }
-    echo "</table>";
+    echo "</table></div>";
 echo "<br><br>";
 # ic_name, oc_fn
+// echo "<button class='button' id='printPageButton' style='width: 300px;' onClick='copyText(\"$emails\")'>Copy Participant E-mails</button><br>";
+// echo "<input type=\"text\" value=\"$emails\" id=\"myInput\">";
+?>
+<script>
+  function copyTo() {
+    // Get the text field
+    var copyText = document.getElementById("myInput");
 
-echo 'Sorteer op:';
-?> 
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
 
-<select id="sort_table" style="padding: 5px; border-radius: 2px; margin-bottom: 1rem;"
-onchange="location.href = '/eoschargen/exports/participants.php?sort=' + this.value; ">
-<option value="">Kies een optie</option>
-<option value="oc_fn asc"> OC Naam (Oplopend)</option>
-<option value="oc_fn desc">OC Naam (Aflopend)</option>
-<option value="ic_name asc">IC Naam (Oplopend)</option>
-<option value="ic_name desc">IC Naam (Aflopend)</option>
-<option value="type asc">Soort Inschrijf (Oplopend)</option>
-<option value="type desc">Soort Inschrijf (Aflopend)</option>
-<option value="register_date asc">Inschrijf Datum (Oplopend)</option>
-<option value="register_date desc">Inschrijf Datum (Aflopend)</option>
-</select>
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  }
+</script>
+<?php 
+$emails = '';
+while ($row4 = mysqli_fetch_array($res4)) {
+  $emails = $emails . $row4['email'].';';
+}
+echo "<input type=\"text\" class=\"hidden-text\" value=\"$emails\" id=\"myInput\">";
+?>
+<button class="button" id="CopyEmailButton" onclick="copyTo()">Copy Participant E-mails</button><br><br>
+<div id="SortBy">Sorteer op:
+  <select id="sort_table" style="padding: 5px; border-radius: 2px; margin-bottom: 1rem;"
+  onchange="location.href = '/eoschargen/exports/participants.php?sort=' + this.value; ">
+    <option value="">Kies een optie</option>
+    <option value="oc_fn asc"> OC Naam (Oplopend)</option>
+    <option value="oc_fn desc">OC Naam (Aflopend)</option>
+    <option value="ic_name asc">IC Naam (Oplopend)</option>
+    <option value="ic_name desc">IC Naam (Aflopend)</option>
+    <option value="type asc">Soort Inschrijf (Oplopend)</option>
+    <option value="type desc">Soort Inschrijf (Aflopend)</option>
+    <option value="register_date asc">Inschrijf Datum (Oplopend)</option>
+    <option value="register_date desc">Inschrijf Datum (Aflopend)</option>
+  </select>
+</div>
 <?php
-    echo "<table>";
-  echo '<th width="20%">OC Name</th>';
-  echo '<th width="20%">IC Name</th>';
-  echo '<th width="20%">Soort Inschrijf</th>';
-  echo '<th width="20%">Inschrif Datum</th>';
-  echo '<th width="30%">Handtekening</th>';
-  echo '<th width="10%">Foto Opt-Out</th>';
+  echo "<table>";
+    echo '<th width="20%">OC Name</th>';
+    echo '<th width="20%">IC Name</th>';
+    echo '<th width="15%">Soort Inschrijf</th>';
+    echo '<th width="10%">Inschrif Datum</th>';
+    echo '<th width="25%">Handtekening</th>';
+    echo '<th width="10%">Foto Opt-Out</th>';
     echo "</tr>";
 
   while ($row = mysqli_fetch_array($res)) {
@@ -198,8 +233,8 @@ onchange="location.href = '/eoschargen/exports/participants.php?sort=' + this.va
     echo '<td>' . $row['ic_name'] . "</td>";
     echo '<td>' . $row['type'] . "</td>";
     echo '<td>' . $row['register_date']. "</td>";
-    echo '<td width="30%" height="40px">&nbsp;</td>'; 
-    echo '<td width="10%" height="40px">&nbsp;</td>'; 
+    echo '<td height="40px">&nbsp;</td>'; 
+    echo '<td height="40px">&nbsp;</td>'; 
     echo "</tr>";
   }
   echo "</table>";
