75323a1ee7bddfb13a1ba0f2a5fb60ff0424ace5
WafflesMcDuff <goldjd@gmail.com>
2024-07-24 18:58:07 +0200

    Add npc file uploader

---
+++ handler/fileupload_npc/save.php
@@ -0,0 +1,7 @@
+<?php
+include_once($_SERVER["DOCUMENT_ROOT"] . "/eoschargen/_includes/config.php");
+$charid = $_POST["charid"];
+$src = $_SERVER["DOCUMENT_ROOT"] . $_POST["image_name"];
+$dst = $APP["root"] . '/img/passphoto/npc/'.$_POST["charid"].'.jpg';
+rename($src,$dst);
+header("Location: ../../index.php?viewChar=".$charid."&editInfo=true");
