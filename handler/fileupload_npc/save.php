54f06f67af236d5feed901deeef8ab39aaffb12b
WafflesMcDuff <goldjd@gmail.com>
2024-07-24 19:16:13 +0200

    Update success redirect

--- handler/fileupload_npc/save.php
+++ handler/fileupload_npc/save.php
@@ -4,4 +4,4 @@ $charid = $_POST["charid"];
 $src = $_SERVER["DOCUMENT_ROOT"] . $_POST["image_name"];
 $dst = $APP["root"] . '/img/passphoto/npc/'.$_POST["charid"].'.jpg';
 rename($src,$dst);
-header("Location: ../../index.php?viewChar=".$charid."&editInfo=true");
+header("Location: index.php");
