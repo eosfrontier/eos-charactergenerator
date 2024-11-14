87cdddcae67d01084905603329ef2b1605d3b2c1
Josh G <goldjd@gmail.com>
2024-11-14 16:06:41 +0100

    Refactor Sleepytime (#43)
    * Aquila Alert working
    
    * Add npc file uploader
    
    * Display existing pic if it exists
    
    * Fix img reference
    
    * Fix img reference
    
    * Fix img reference
    
    * Update text
    
    * Update success redirect
    
    * Add heading
    
    * Fix Header levels
    
    * Active faction table works and links to active characters list
    
    * Show when player is active since
    
    * Include characterID in get_active_players to enable deactivate button
    
    * Deactivate working
    
    * Add pasfoto to confirm page
    
    * Add pasfoto to confirm page
    
    * Add pasfoto to confirm page
    
    * Hide deactivate button when printing
    
    * Spelling fix
    
    * Add latest event and split playercap functions into separate function file
    
    * Use integers
    
    * Update participants.php
    
    * integrate tidy into player cap (#37)
    
    * Specials Participant List is now also sign-in-sheet
    
    * Add registrants list for Frontier
    
    * last tweak for reg list
    
    * start adding display sheet
    
    * Try sheet display
    
    * Remove print button
    
    * Remove tear strips
    
    * Add skills to character sheets, revise inschrijflijst
    
    * fix columns
    
    * Table Width
    
    * Add sort-by dropdown
    
    * Remove echo tablesort
    
    * Fix table header
    
    * Fix bugs, finalize skill_desc v0.1
    
    * Add skill links to augment
    
    * Add line breaks retrieved from SQL when displaying skill description
    
    * Skill desc styling
    
    * Try responsive skill desc
    
    * Try REM instead
    
    * Increase mobile font size
    
    * Apply chargen style to skill desc
    
    * button relative width
    
    * Fix logo position on sheet
    
    * Fix logo position on sheet
    
    * Fix my dumb mistake
    
    * Last tweak
    
    * Update participants.php
    
    * Add E-mail export button
    
    * Dropdowns show selected option
    
    * Final tweaks
    
    * Autoformat EVERYTHING
    
    * Show file not yet uploaded instead of broken link
    
    * Modify pic upload from 200x200 to 300x400
    
    * Add download buttton
    
    * Download images button is working
    
    * Remove unused file
    
    * Add Export to CSV function
    
    * Final cleanup
    
    * Fix MIME type for CSV
    
    * Add sponsor ticket counts to registrant list
    
    * Add donations to participants page
    
    * Refactor of sleepytime
    
    * Last fix
    
    * OK really fixed
    
    * Change style
    
    ---------
    
    Co-authored-by: Goldberg, Joshua <jogoldberg@deloitte.nl>

--- exports/sleepytime/room-signs.php
+++ exports/sleepytime/room-signs.php
@@ -1,5 +1,10 @@
+<?php
+        header("Content-Type: text/html; charset=ISO-8859-1");
+        include_once ($_SERVER["DOCUMENT_ROOT"] . '/eoschargen/db.php');
+        require './buildtable.php';
+        ?>
 <!doctype html>
-<html lang="en">
+<lang="en">
 
 <head>
     <title>Room Signs</title>
@@ -10,146 +15,40 @@
 
 <body>
     <div>
-        <?php
-        header("Content-Type: text/html; charset=ISO-8859-1");
-        include_once ($_SERVER["DOCUMENT_ROOT"] . '/eoschargen/db.php');
-
-        $bldg_sql = "SELECT field_value from `jml_eb_field_values` where field_id = 36 AND field_value !='medische uitzondering \"Geregeld met Orga\"' GROUP by field_value";
-        $bldg_res = $UPLINK->query($bldg_sql);
-        //$row = mysqli_fetch_assoc($res);
         
-        while ($bldg_row = mysqli_fetch_assoc($bldg_res)) {
-            $building = $bldg_row['field_value'];
-            //This makes the list of rooms
-            $room_sql = "SELECT CONCAT(coalesce(v3.field_value,''),coalesce(v4.field_value,'')) as room from joomla.jml_eb_registrants r
-                left join joomla.jml_eb_field_values v2 on (v2.registrant_id = r.id and v2.field_id = 36)
-                left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 93)
-                left join joomla.jml_eb_field_values v7 on (v7.registrant_id = r.id and v7.field_id = 94)
-                left join jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
-                left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 37)
-                left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 38)
-                where v2.field_value ='$building' AND v5.field_value = 'Speler' AND r.event_id = $EVENTID and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal' OR r.payment_method = 'os_bancontact')) OR
-                (r.published in (0,1) AND r.payment_method = 'os_offline')) AND (v6.field_value = 'No' OR v6.field_value IS NULL) AND (v7.field_value = 'No' OR v7.field_value IS NULL)
-                UNION DISTINCT
-                select CONCAT(coalesce(v3.field_value,''),coalesce(v4.field_value,''),coalesce(v6.field_value,''),coalesce(v7.field_value,'')) as room from joomla.jml_eb_registrants r
-                left join joomla.jml_eb_field_values v2 on (v2.registrant_id = r.id and v2.field_id = 93)
-                left join jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
-                left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 37)
-                left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 38)
-                left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 72)
-                left join joomla.jml_eb_field_values v7 on (v7.registrant_id = r.id and v7.field_id = 73)
-                where 'Bastion' = '$building' AND 
-                ASCII(UPPER(LEFT(CONCAT(coalesce(v3.field_value,''),coalesce(v4.field_value,''),coalesce(v6.field_value,''),coalesce(v7.field_value,'')),1))) 
-                BETWEEN 64 AND 90 AND v5.field_value != 'Speler' AND r.event_id = $EVENTID and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal' OR r.payment_method = 'os_bancontact')) OR
-                (r.published in (0,1) AND r.payment_method = 'os_offline'))
-                UNION DISTINCT
-                select CONCAT(coalesce(v3.field_value,''),coalesce(v4.field_value,''),coalesce(v6.field_value,''),coalesce(v7.field_value,'')) as room from joomla.jml_eb_registrants r
-                left join joomla.jml_eb_field_values v2 on (v2.registrant_id = r.id and v2.field_id = 36)
-                left join jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
-                left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 37)
-                left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 38)
-                left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 72)
-                left join joomla.jml_eb_field_values v7 on (v7.registrant_id = r.id and v7.field_id = 73)
-                where 'tweede gebouw' = '$building' AND 
-                ASCII(UPPER(LEFT(CONCAT(coalesce(v3.field_value,''),coalesce(v4.field_value,''),coalesce(v6.field_value,''),coalesce(v7.field_value,'')),1))) 
-                NOT BETWEEN 64 AND 90 AND v5.field_value != 'Speler' AND r.event_id = $EVENTID and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal' OR r.payment_method = 'os_bancontact')) OR
-                (r.published in (0,1) AND r.payment_method = 'os_offline'))
-                UNION DISTINCT
-                select trim(substring_index(LEFT(v6.field_value,LOCATE(' - ',v6.field_value) - 1),',',-1)) as room from joomla.jml_eb_registrants r
-                left join joomla.jml_eb_field_values v1 on (v1.registrant_id = r.id and v1.field_id = 21)
-                left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 37)
-                left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 38)
-                left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 71)
-                where LEFT(v6.field_value,LOCATE(',',v6.field_value) - 1) = '$building' AND r.event_id = $EVENTID
-                and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal' OR r.payment_method = 'os_bancontact')) OR
-                (r.published in (0,1) AND r.payment_method = 'os_offline'))
-                GROUP by room ORDER by room;";
-            $room_res = $UPLINK->query($room_sql);
-
-            while ($room_row = mysqli_fetch_assoc($room_res)) {
-                $room = $room_row['room'];
-                $sql = "SELECT r.id, v6.field_value as foodlocation, (SELECT character_name from ecc_characters WHERE ecc_characters.characterID = substring_index(v1.field_value, ' - ', -1)) as name, 
-                        v2.field_value as building, CONCAT(coalesce(v3.field_value,''),coalesce(v4.field_value,'')) as room from joomla.jml_eb_registrants r
-                        left join joomla.jml_eb_field_values v1 on (v1.registrant_id = r.id and v1.field_id = 21)
-                        left join joomla.jml_eb_field_values v2 on (v2.registrant_id = r.id and v2.field_id = 36)
-                        left join jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
-                        left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 37)
-                        left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 38)
-                        left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 58)
-                        left join joomla.jml_eb_field_values v7 on (v7.registrant_id = r.id and v7.field_id = 71)
-                        where CONCAT(coalesce(v3.field_value,''),coalesce(v4.field_value,'')) = '$room' AND v2.field_value = '$building' 
-                        AND v5.field_value = 'Speler' AND v7.field_value is NULL AND r.event_id = $EVENTID and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal' OR r.payment_method = 'os_bancontact')) OR
-                        (r.published in (0,1) AND r.payment_method = 'os_offline'))AND v2.field_value NOT LIKE 'medische%'
-
-                        UNION
-
-                        SELECT r.id, v6.field_value as foodlocation, CONCAT(v5.field_value,' ',r.first_name, ' ', 
-                        COALESCE(v2.field_value,''),' ', SUBSTRING(r.last_name,1,1),'.') as name, 'tweede gebouw' as building, CONCAT(COALESCE(v4.field_value,''),
-                        COALESCE(v3.field_value,'')) as room from joomla.jml_eb_registrants r
-                        left join joomla.jml_eb_field_values v2 on (v2.registrant_id = r.id and v2.field_id = 16)
-                        left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 73)
-                        left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 72)
-                        left join joomla.jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
-                        left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 58)
-                    where v5.field_value != 'Speler' AND ASCII(UPPER(LEFT(CONCAT(COALESCE(v4.field_value,''), COALESCE(v3.field_value,'')),1))) 
-                    NOT BETWEEN 64 AND 90 AND 'tweede gebouw' =  '$building' AND r.event_id = $EVENTID and (v3.field_value = '$room' or v4.field_value = '$room') AND
-                    ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal' OR r.payment_method = 'os_bancontact')) OR  (r.published in (0,1) AND r.payment_method = 'os_offline'))
-                UNION
-                SELECT r.id, v6.field_value as foodlocation, CONCAT(v5.field_value,' ',r.first_name, ' ', COALESCE(v2.field_value,''),' ', 
-                SUBSTRING(r.last_name,1,1),'.') as name, 'Bastion' as building, CONCAT(COALESCE(v4.field_value,''),COALESCE(v3.field_value,'')) as room 
-                from joomla.jml_eb_registrants r
-                    left join joomla.jml_eb_field_values v2 on (v2.registrant_id = r.id and v2.field_id = 16)
-                    left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 73)
-                    left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 72)
-                    left join joomla.jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
-                    left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 58)
-                where v5.field_value != 'Speler' AND ASCII(UPPER(LEFT(CONCAT(COALESCE(v4.field_value,''),COALESCE(v3.field_value,'')),1))) 
-                BETWEEN 64 AND 90 AND 'Bastion' = '$building' AND r.event_id = $EVENTID and (v3.field_value = '$room' or v4.field_value = '$room') AND
-                ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal' OR r.payment_method = 'os_bancontact')) OR  (r.published in (0,1) AND r.payment_method = 'os_offline'))
-
-                UNION
-
-                select r.id, v7.field_value as foodlocation, if(v5.field_value = 'Speler', (SELECT character_name from ecc_characters WHERE ecc_characters.characterID = substring_index(v1.field_value, ' - ', -1)),CONCAT(v5.field_value,' ',r.first_name, ' ', COALESCE(v2.field_value,''),' ', SUBSTRING(r.last_name,1,1),'.')) as name,
-                LEFT(v6.field_value,LOCATE(',',v6.field_value) - 1) as building, substring_index(LEFT(v6.field_value,LOCATE(' - ',v6.field_value) - 1),',',-1) as room 
-                from joomla.jml_eb_registrants r
-                    left join joomla.jml_eb_field_values v1 on (v1.registrant_id = r.id and v1.field_id = 21)
-                    left join joomla.jml_eb_field_values v2 on (v2.registrant_id = r.id and v2.field_id = 16)
-                    left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 37)
-                    left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 38)
-                    left join joomla.jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
-                    left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 71)
-                    left join joomla.jml_eb_field_values v7 on (v7.registrant_id = r.id and v7.field_id = 58)
-                where LEFT(v6.field_value,LOCATE(',',v6.field_value) - 1) = '$building' AND trim(substring_index(LEFT(v6.field_value,LOCATE(' - ',v6.field_value) - 1),',',-1)) = '$room' AND r.event_id = $EVENTID
-                and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal' OR r.payment_method = 'os_bancontact')) OR
-                (r.published in (0,1) AND r.payment_method = 'os_offline'))
-                ORDER by building, room, name;";
-
-                $res = $UPLINK->query($sql);
+        <?php
+        $buildings = GetBuildings($sleepers);
+        foreach ($buildings as $building){
+            $rooms = GetRooms($sleepers,$building);
+            foreach ($rooms as $room){
                 echo "<div class='roomsign' style=''>";
                 echo '<p><button class="button" id="printPageButton" style="width: 100%;" onClick="window.print();">Print</button></p>';
                 echo "<center class='center'><font face='Orbitron' size=15><br>" . str_replace('tweede gebouw', 'Zonnedauw', $building) . "<br>$room<br></font></center>";
+                $room_sleepers = BreakdownByRoom($sleepers,$building,$room);
                 ?>
                 <table>
                     <tr>
-                    <th><center>Name</center></th>";
-                <th><center>Room Clean Sign-Off</center></th>
-                    </tr>
-                <?php
-                while ($row = mysqli_fetch_array($res)) {
-                    //     $foodlocation = $row['foodlocation'];
-                    //     if ($foodlocation == '') {
-                    //         $foodlocation = $building;
-                    //     } else {
-                    //         if ($foodlocation = 'tweede gebouw') {
-                    //             $foodlocation = 'Zonnedauw';
-                    //         } else {
-                    //         $foodlocation = $row['foodlocation'];
-                    //         }
-                    //     }
+                    <th><center>Name</center></th>
+                    <th><center>Room Clean Sign-Off</center></th>
+                    <?php if (isset($use_food_loc) && $use_food_loc == 'true'){ ?>
+                    <th><center>Eating Location</center></th>
+                    <?php }
+                    echo '</tr>';
+                    foreach ($room_sleepers as $sleeper){
+                        if (isset($use_food_loc) && $use_food_loc == 'true'){
+                            if (isset($sleeper['food_loc'])){
+                                $foodlocation = $sleeper['food_loc'];
+                            }
+                            else {
+                                $foodlocation = $building;
+                            }
+                    }
                     echo "<tr>
-                    <td>" . $row['name'] . "</td>";
-                    //     echo "<td><center>" . str_replace('tweede gebouw', 'Zonnedauw', $foodlocation) . "</center></td>";
+                    <td>" . $sleeper['Name'] . "</td>";
                     echo "<td><center>&nbsp;</center></td>";
+                    if (isset($use_food_loc) && $use_food_loc == 'true'){
+                        echo "<td><center>" . str_replace('tweede gebouw', 'Zonnedauw', $foodlocation) . "</center></td>";
+                        }
                     echo "</tr>";
                 }
                 echo "</table>";
@@ -159,7 +58,7 @@
                     <tr>
                         <td>
                             <ul>
-                                <li>Please nominate 1 person from your room to do the final clean-and-swept check. They should
+                                <li>Choose 1 person from your room to do the final clean-and-swept check. They should
                                     sign next to their name when the room is clean and ready to be checked.</li>
                             </ul>
                         </td>
@@ -196,9 +95,5 @@
                 echo "</div>";
             }
         }
-        ?>
-
-    </div>
-</body>
-
-</html>
+            ?>
+                
