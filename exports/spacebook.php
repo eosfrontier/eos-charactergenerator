2e3e81a5cb6589e778ceeca986a7b524bfaf7fa0
WafflesMcDuff <goldjd@gmail.com>
2024-11-14 23:53:21 +0100

    Fix image size

--- exports/spacebook.php
+++ exports/spacebook.php
@@ -57,7 +57,7 @@ $notCancelled = "((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payme
           }
           
           echo "<div style=\"padding: 5px; float: left; height: 86px; width: 100%;\">"
-            . "<img src=\"../img/passphoto/" . $row['characterID'] . ".jpg\" style=\"height: 90px; float: left; border-radius: 100%;\" />"
+            . "<img src=\"../img/passphoto/" . $row['characterID'] . ".jpg\" style=\"width: 90px; float: left; border-radius: 100%;\" />"
 
             . "<p style=\"position: relative; padding-left: 5px;\">"
             . "<strong>" . ucfirst($row['character_name']) . "</strong>"
