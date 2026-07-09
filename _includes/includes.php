c580d44fd5f2b27713b2ecf58be3c0b6082e1cba
WafflesMcDuff <goldjd@gmail.com>
2026-07-09 14:04:02 +0200

    Enable output buffering

--- _includes/includes.php
+++ _includes/includes.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 include_once __DIR__ . '/class_db.php';
 include_once __DIR__ . '/config.php';
 include_once __DIR__ . '/../db.php';// include the db.php connection. This file is not available on my github for security reasons.
